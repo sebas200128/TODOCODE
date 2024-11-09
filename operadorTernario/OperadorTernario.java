ec063381c2ab606574b49cfd597b3375086c8c4e
sebas200128 <111097248+sebas200128@users.noreply.github.com>
2024-11-08 20:10:50 -0500

    Nueva actualizacion en la clase OperadorTernario

--- operadorTernario/OperadorTernario.java
+++ operadorTernario/OperadorTernario.java
@@ -15,14 +15,19 @@ public class OperadorTernario {
     public static void main(String[] args) {
         double prom;
         String condiFinal;
+        String nombre;
+        
 
         Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Cual es tu nombre? ");
+        nombre = leer.nextLine();
 
         System.out.println("Ingrese el promedio del alumno: ");
         prom = leer.nextDouble();
 
         condiFinal = (prom >= 11) ? "Aprobado" : "Desaprobado";
         
-        System.out.println("La condicion final del alumno es: "+condiFinal+" El promedio fue: "+prom);
+        System.out.println("Alumno: "+nombre+" Su condicion final del alumno es: "+condiFinal+" El promedio fue: "+prom);
     }
 }
